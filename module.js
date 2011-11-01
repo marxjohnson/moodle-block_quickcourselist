88d6c9b7234962054eabfe2aa8af81958b8f644e
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-11-01 15:40:08 +0000

    Tidied up Javascript a bit

--- module.js
+++ module.js
@@ -2,27 +2,23 @@ M.block_quickcourselist = {
     init: function(Y, instanceid) {
         this.Y = Y;
         this.instanceid = instanceid;
-                
+
         this.progress = Y.one('#quickcourseprogress');
         this.xhr = null;
-        
-        Y.on('keyup', this.search_on_type, '#quickcourselistsearch');
-        Y.on('submit', this.search_on_submit, '#quickcourseform');
-    },
 
-    search_on_type: function(e) {
-        var searchstring = e.target.get('value');
-        M.block_quickcourselist.search(searchstring);
-    },
-
-    search_on_submit: function(e) {
-        e.preventDefault();
-        var searchstring = e.target.getById('quickcourselistsearch').get('value');
-        M.block_quickcourselist.search(searchstring);
+        Y.one('#quickcourselistsearch').on('keyup', function(e) {
+            var searchstring = e.target.get('value');
+            M.block_quickcourselist.search(searchstring);
+        });
+        Y.one('#quickcourseform').('submit', function(e) {
+            e.preventDefault();
+            var searchstring = e.target.getById('quickcourselistsearch').get('value');
+            M.block_quickcourselist.search(searchstring);
+        });
     },
 
     search: function(string) {
-        
+
         var Y = this.Y;
         uri = M.cfg.wwwroot+'/blocks/quickcourselist/quickcourse.php';
         if (this.xhr != null) {
@@ -36,10 +32,10 @@ M.block_quickcourselist = {
                     var courses = Y.JSON.parse(o.responseText);
                     var block = M.block_quickcourselist;
                     list = Y.Node.create('<ul />');
-                    if (courses.length > 0) {                        
-                        for (c in courses) {
-                            list.appendChild(Y.Node.create('<li><a href="'+M.cfg.wwwroot+'/course/view.php?id='+courses[c].id+'">'+courses[c].shortname+' '+courses[c].fullname+'</a></li>'));
-                        }                     
+                    if (courses.length > 0) {
+                        Y.Array.each(courses, function(course) {
+                            Y.Node.create('<li><a href="'+M.cfg.wwwroot+'/course/view.php?id='+course.id+'">'+course.shortname+' '+course.fullname+'</a></li>').appendTo(list);
+                        });
                     }
                     Y.one('#quickcourselist').replace(list);
                     list.setAttribute('id', 'quickcourselist');
@@ -51,7 +47,7 @@ M.block_quickcourselist = {
                         block.progress.setStyle('visibility', 'hidden');
                         if (o.statusText !== undefined) {
                             var list = Y.Node.create('<p>'+o.statusText+'</p>');
-                            block.list.replace(list);
+                            Y.one('#quickcourselist').replace(list);
                             list.set('id', 'quickcourselist');
                         }
                     }
