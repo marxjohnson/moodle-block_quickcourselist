2bb621908c39c999818afa23f4ed225d08191ff0
Alexander Kiy <alekiy@uni-potsdam.de>
2013-10-14 18:52:09 +0200

    added german translation

---
+++ lang/de/block_quickcourselist.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Defines lang strings for Quick Course List
+ *
+ * @package    blocks
+ * @subpackage  quickcourselist
+ * @author      Alexander Kiy <alekiy@uni-potsdam.de>
+ * @copyright   2013 University of Potsdam, GER
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['blockname']         = 'Kurssuche';
+$string['loading']  = 'Lade...';
+$string['pluginname']   =   'Kurssuche';
+$string['pluginnameplural']   =   'Kurssuche';
+$string['quickcourselist:use']         = 'Kurssuche benutzen';
+$string['quickcourselist']         = 'Kurssuche';
