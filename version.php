175c98bda3880f6877086394ce99014efe0cb3a7
Mark Johnson <mark.johnson@it.ox.ac.uk>
2015-04-25 16:32:22 +0100

    Updated version.php

--- version.php
+++ version.php
@@ -24,8 +24,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2014032402;
-$plugin->requires = 2010112400;
+$plugin->version = 2015042500;
+$plugin->requires = 2014051200;
 $plugin->component = 'block_quickcourselist';
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '2.0.2dev';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.1';
+
